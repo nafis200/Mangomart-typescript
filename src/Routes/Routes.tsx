9848fc15ea6980448955f292936d6b18d4b5d373
nafis200 <nafisahamed14@gmail.com>
2024-09-30 19:55:38 +0600

    singlePage done

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,35 +1,38 @@
-import {
-    createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import Login from "../components/login/Login";
 import Register from "../components/register/Register";
 // import Register from "../components/register/Register"
 // import Login from "../components/login/Login";
-import OurMango from './../components/ourmango/OurMango';
+import OurMango from "../components/ourmango/OurMango";
+import SingleMango from '../components/ourmango/SingleMango';
 
 export const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
         path: "/",
-        element:<Main></Main>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-            {
-                path:'/login',
-                element:<Login></Login>
-            },
-            {
-                path:'/register',
-                element:<Register></Register>
-            },
-            {
-                path:'/ourmango',
-                element:<OurMango></OurMango>
-            },
-        ]
-    },
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/ourmango",
+        element: <OurMango></OurMango>,
+      },
+      {
+        path:'/singlemango/:id',
+        element:<SingleMango></SingleMango>
+      }
+    ],
+  },
 ]);
---
+++ src/components/ourmango/SingleMango.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { useParams } from "react-router";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { NavLink } from "react-router-dom";
+
+interface Mango {
+  _id: string;
+  name: string;
+  amount: number;
+  quantity: number;
+  location: string;
+  image: string;
+  description: string;
+}
+
+const SingleMango = () => {
+    const {id} = useParams()
+    const axiosPublic = useAxiosPublic();
+    const {
+      data: mango = [] as Mango[],
+      isLoading,
+      refetch,
+    } = useQuery<Mango[]>({
+      queryKey: ["mango"],
+      queryFn: async () => {
+        const res = await axiosPublic.get<Mango[]>('/mangoInformation');
+        return res.data;
+      },
+    });
+  
+    if (isLoading) {
+      return <div>Loading.....</div>;
+    }
+
+    const Mangos = mango.filter((item)=> item._id === id) 
+  return(
+    <div>
+       <h1 className="text-white h-24"></h1>
+       <div className="lg:flex md:flex space-x-7">
+         <div>
+         <img 
+          src={`${Mangos[0].image}`} className="" alt="" />
+         </div>
+         <div className="mt-5">
+           <h1>Name: <span className="font-bold">{Mangos[0].name}</span> </h1>
+           <h1>location: <span className="font-bold">{Mangos[0].location}</span> </h1>
+           <h1>Description: <span className="font-bold">{Mangos[0].description}</span> </h1>
+           <h1>Price: <span className="font-bold">{Mangos[0].amount} per kg</span> </h1>
+           <NavLink to="/ourmango"> <button className="btn btn-success mt-5">Go back</button> </NavLink>
+         </div>
+       </div>
+    </div>
+  )
+};
+
+export default SingleMango;
