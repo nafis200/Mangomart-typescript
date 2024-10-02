ece0f12514876f4190cb35484ffa13c2fe47146b
nafis200 <nafisahamed14@gmail.com>
2024-10-02 22:57:29 +0600

    sslcommerce done

--- src/Pages/Dashboard/payment/Paymentsuccess.jsx
+++ src/Pages/Dashboard/payment/Paymentsuccess.jsx
@@ -1,13 +1,20 @@
 
 
 import React from 'react';
+import { useNavigate } from 'react-router';
 
 const Paymentsuccess = () => {
+    const navigate= useNavigate()
     return (
         <div className=''>
             <p className='h-40'></p>
             <p className='text-center font-bold'>Successfully payment thank you</p>
             <p className='text-center font-bold'>Thank you</p>
+            <div className='flex justify-center mt-10'>
+            <button onClick={()=>{
+                 navigate('/')
+            }} className='btn'>Go back</button>
+            </div>
         </div>
     );
 };
