10f522666b82f996dc27968af7b7915a0b87b804
nafis200 <nafisahamed14@gmail.com>
2024-10-03 21:56:22 +0600

    animation added

--- src/components/ordermango/OrderMango.tsx
+++ src/components/ordermango/OrderMango.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
 import { NavLink } from "react-router-dom";
+import 'animate.css';
 
 interface Mango {
     _id: string;
@@ -34,14 +35,16 @@ const OrderMango: React.FC = () => {
     return (
         <div>
       <h1 className="h-24 text-white">text</h1>
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 space-x-4 mt-5 space-y-7">
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 space-x-4 mt-5 space-y-7 animate__animated animate__fadeInDownBig" style={{ animationDuration: '2s' }}>
       {mango?.map((item, index) => {
         return(
           <div className="card bg-green-100 bg-base-100 shadow-xl mt-10 hover:bg-yellow-400 hover:shadow-black">
           <figure>
             <img
               src={`${item.image}`}
+              className='animate__animated animate__fadeInDownBig animate__delay-1s'
               alt="Shoes"
+              style={{ animationDuration: '2s' }}
             />
           </figure>
           <div className="card-body">
